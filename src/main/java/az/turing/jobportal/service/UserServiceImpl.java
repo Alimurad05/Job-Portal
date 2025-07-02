54648dc7d5457c5930906b87b68bdb0e4f0b16c6
user <alimuradof93@gmail.com>
2025-07-02 18:52:09 +0400

    security has been added

--- src/main/java/az/turing/jobportal/service/UserServiceImpl.java
+++ src/main/java/az/turing/jobportal/service/UserServiceImpl.java
@@ -5,15 +5,19 @@ import az.turing.jobportal.entity.User;
 import az.turing.jobportal.exception.JobPortalException;
 import az.turing.jobportal.repo.UserRepository;
 import az.turing.jobportal.utility.Utilities;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
 
     @Override
     public UserDTO registerUser(UserDTO userDTO) throws JobPortalException {
@@ -21,6 +25,7 @@ public class UserServiceImpl implements UserService {
             throw new IllegalArgumentException("Email already exists");
         }
         userDTO.setId(Utilities.getNextSequence("users"));
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         User user=userDTO.toEntity();
         user=userRepository.save(user);
         return user.toDto();
